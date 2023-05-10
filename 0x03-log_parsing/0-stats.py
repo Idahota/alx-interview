a4de8be739313f86acc4f324467817a520550aba
idahota <akpasubi_v@yahoo.com>
2023-05-09 20:25:00 -0700

    Update files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
-""" This scripts read each line from the stdin and calculates the meterics """
+
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
 
-def print__http_status_logs(dic, size):
-    """ Prints information """
+
+def printsts(dic, size):
+    """ WWPrints information """
     print("File size: {:d}".format(size))
     for i in sorted(dic.keys()):
         if dic[i] != 0:
             print("{}: {:d}".format(i, dic[i]))
 
-http_status_array = {
-    "200": 0, 
-    "301": 0, 
-    "400": 0, 
-    "401": 0, 
-    "403": 0,
-    "404": 0, 
-    "405": 0, 
-    "500": 0
-}
+
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
 
 try:
-    for ln in sys.stdin:
+    for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            print__http_status_logs(http_status_array, size)
-        status_list = ln.split()
+            printsts(sts, size)
+
+        stlist = line.split()
         count += 1
+
         try:
-            size += int(status_list[-1])
+            size += int(stlist[-1])
         except:
             pass
+
         try:
-            if status_list[-2] in http_status_array:
-                http_status_array[status_list[-2]] += 1
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
         except:
             pass
-    print__http_status_logs(http_status_array, size)
+    printsts(sts, size)
+
+
 except KeyboardInterrupt:
-    print__http_status_logs(http_status_array, size)
+    printsts(sts, size)
     raise
